9293995a643423aa3f5a584c37d6c7d0d6b3f916
Michael Koenig <michael.koenig2@student.kit.edu>
2017-09-26 02:38:46 +0200

    support punycode-domains (resolve IP via host)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ IP=`curl http://icanhazip.com/`
 trap exit SIGHUP SIGINT SIGTERM
 
 function issue_cert () {
-  if [ "$(ping -c1 -n $1 | head -n1 | sed 's/.*(\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\)).*/\1/g')" == "$IP" ]; then
+  if [ "$(host -t A $1|awk '{print $NF}'|head -n1)" == "$IP" ]; then
     acme.sh \
       --home /usr/bin \
       --issue \
